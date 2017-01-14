2ac1d1ac10a05133d5c824a156be716f35a46791
Thomas Alberola <thomas@needacoffee.fr>
2017-01-14 17:42:00 +0100

    creating essential directory

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const chat = jcmp.events.Call('get_chat')[0];
 
-const message = 'Bienvenu sur Rail de cake !';
+const message = "Bienvenue sur Rail de cake ! Le serveur est en cours d'amélioration. Le serveur redemarre automatiquement en cas de crash ;-). Pour le chat vocal et les nouvelles rejoins notre Discord : https://raildecake.fr/discord";
 const messageColor = new RGB(255, 140, 0);
 
 jcmp.events.Add("PlayerReady", (player) => {
